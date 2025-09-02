8b1121e1f8ddeb7b6536754fc17439eca263e725
Aus <vargazalan58@gmail.com>
2025-09-02 21:29:40 +0200

    PAW-1: Update dependencies

--- app/src/main/java/com/codingcanines/howliday/MainActivity.kt
+++ app/src/main/java/com/codingcanines/howliday/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             HowlidayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Aus",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
